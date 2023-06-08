f1248f99dc4a9fece0c9c8afb11e91822ffc71c8
Your Name <samtshiek@yahoo.fr>
2023-06-08 15:59:20 -0700

    Fixed friend wall clipping

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,15 +10,15 @@ public class MainMenu : MonoBehaviour
     public void StartLhon ()
     {
         audioSource.PlayOneShot(Resources.Load<AudioClip>("dogBark"));
-        //SceneManager.LoadScene(1);
-        SceneManager.LoadScene("EBExperience");
+        SceneManager.LoadScene(1);
+        //SceneManager.LoadScene("EBExperience");
     }
 
     public void StartHike()
     {
         audioSource.PlayOneShot(Resources.Load<AudioClip>("harpChimes"));
-        //SceneManager.LoadScene(2);
-        SceneManager.LoadScene("EBHike");
+        SceneManager.LoadScene(2);
+        //SceneManager.LoadScene("EBHike");
     }
 
     public void Quit ()
